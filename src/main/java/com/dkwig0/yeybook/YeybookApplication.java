6cbed4052e3647c8fc73d21794970ba5ceb3a382
dkwigo <tsvirkoaliaksei@gmail.com>
2021-01-11 11:51:48 +0100

    java-start

---
+++ src/main/java/com/dkwig0/yeybook/YeybookApplication.java
@@ -0,0 +1,13 @@
+package com.dkwig0.yeybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeybookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeybookApplication.class, args);
+    }
+
+}
