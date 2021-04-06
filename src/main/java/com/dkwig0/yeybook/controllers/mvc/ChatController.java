d87ddff51c82beb70e2034b235262c6a275b0728
dkwigo <tsvirkoaliaksei@gmail.com>
2021-04-06 12:45:04 +0200

    User registration

--- src/main/java/com/dkwig0/yeybook/controllers/mvc/ChatController.java
+++ src/main/java/com/dkwig0/yeybook/controllers/mvc/ChatController.java
@@ -1,12 +1,23 @@
 package com.dkwig0.yeybook.controllers.mvc;
 
+import com.dkwig0.yeybook.jpa.entities.Role;
+import com.dkwig0.yeybook.jpa.entities.User;
+import com.dkwig0.yeybook.jpa.repositories.UserRepository;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
 
 @Controller
 public class ChatController {
+
+    @Autowired
+    UserRepository ur;
 
     @GetMapping
     public String mainChatPage() {
@@ -21,6 +32,18 @@ public class ChatController {
     @GetMapping("sound.wav")
     public @ResponseBody byte[] sound() throws Exception {
         return IOUtils.toByteArray(getClass().getResourceAsStream("/static/wav/sound.wav"));
+    }
+
+    @GetMapping("registration")
+    public String registrationPage() {
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String signUserUp(@RequestParam(name = "username", required = true) String username,
+                           @RequestParam(name = "password", required = true) String password) {
+        ur.save(new User(username, password, true, Collections.singleton(Role.USER)));
+        return loginPage();
     }
 
 }
